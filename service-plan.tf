58072b48715bf2ecea67739aaa2196de36430b56
xenedium <ahmed@abderraziq.com>
2023-03-26 21:08:01 +0000

    Changed sku_plan to min required

--- service-plan.tf
+++ service-plan.tf
@@ -3,5 +3,5 @@ resource "azurerm_service_plan" "az-serverless-terraform" {
   resource_group_name = azurerm_resource_group.az-serverless-terraform.name
   location            = azurerm_resource_group.az-serverless-terraform.location
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "Y1"
 }
